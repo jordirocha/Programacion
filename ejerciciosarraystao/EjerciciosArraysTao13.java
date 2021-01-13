7eda360b47aa6fe5aced4edaf3d4fc49cb009d58
jordirocha <44584534+jordirocha@users.noreply.github.com>
2021-01-13 21:26:41 +0000

    Add files via upload

---
+++ ejerciciosarraystao/EjerciciosArraysTao13.java
@@ -0,0 +1,30 @@
+package ejerciciosarraystao;
+
+import java.util.Scanner;
+
+public class EjerciciosArraysTao13 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce número ");
+        int V = sc.nextInt();
+
+        System.out.print("Introduce el valor aritmetico: ");
+        int I = sc.nextInt();
+
+        System.out.print("¿De qué tamaño? ");
+        int N = sc.nextInt();
+
+        int aritmetica[] = new int[N];
+        aritmetica[0] = V; // Le asigno a la posicion 0 el valor de I
+
+        for (int i = 1; i < aritmetica.length; i++) { // Empiezo desde 1
+            aritmetica[i] = aritmetica[i - 1] + I; // Aquí sumo una posicion anterior más I
+            System.out.println(aritmetica[i]);
+        }
+
+    }
+
+}
